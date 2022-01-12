a8f020c66cbe8fd8290530c7a6b4dc6c54b08a97
Kresna <satyakresna@unud.ac.id>
2022-01-12 16:26:10 +0800

    feat(SSOService): add roles when get user profile

--- src/Services/SSOService.php
+++ src/Services/SSOService.php
@@ -326,6 +326,12 @@ class SSOService
             $user = $response->getBody()->getContents();
             $user = json_decode($user, true);
 
+            // Get roles
+            $roles = ['roles' => []];
+            $roles = $token->parseAccessToken()['resource_access'][$_ENV['SSO_CLIENT_ID']];
+            
+            $user = array_merge($user, $roles);
+
             // Validate retrieved user is owner of token
             $token->validateSub($user['sub'] ?? '');
         } catch (GuzzleException $e) {
