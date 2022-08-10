27cbe3bcd5b19b0c62a35a34046ecddd8e92e138
Satya Kresna <satyakresna@unud.ac.id>
2022-08-10 10:51:59 +0800

    [feat] add input options for copy sso files

--- src/Commands/Copy.php
+++ src/Commands/Copy.php
@@ -4,6 +4,7 @@ namespace RistekUSDI\SSO\Commands;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Illuminate\Filesystem\Filesystem;
 
@@ -14,14 +15,25 @@ class Copy extends Command
      *
      * @var string
      */
-    protected static $defaultName = 'copy:ci3';
+    protected static $defaultName = 'copy:file';
 
     /**
      * The command description shown when running "php bin/sso list".
      *
      * @var string
      */
-    protected static $defaultDescription = 'Copy SSO files for CodeIgniter 3.x!';
+    protected static $defaultDescription = 'Copy SSO files!';
+
+    protected function configure(): void
+    {
+        $this->addOption(
+            'type',
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Which type you want to Copy SSO file',
+            ['php', 'ci3']
+        );
+    }
 
     /**
      * Execute the command
@@ -32,8 +44,15 @@ class Copy extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/ci3', 'application');
-        $output->writeln('Copy success!');
+        $type = $input->getOption('type');
+        if ($type === "php") {
+            (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/php', 'sso');
+            $output->writeln('Copy success!');
+        } else if ($type === "ci3") {
+            (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/ci3', 'application');
+            $output->writeln('Copy success!');
+        }
+
         return Command::SUCCESS;
     }
 }
