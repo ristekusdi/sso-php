60e82bc7a0a4d2d788b1b792d5a0baf72ed99190
Kresna Satya <kresnasatya72@gmail.com>
2024-03-14 11:57:23 +0800

    update stubs for Webauth

--- stubs/ci3/controllers/Webauth.php
+++ stubs/ci3/controllers/Webauth.php
@@ -28,6 +28,9 @@ class Webauth extends CI_Controller {
     {
         $sso = new SSOService;
         $url = $sso->getLogoutUrl();
+        // NOTE: forgetToken must be after getLogoutUrl().
+        // Otherwise the logout form will show error message: id_token_hint not found!
+        $sso->forgetToken();
         return redirect($url);
     }
 
@@ -54,48 +57,76 @@ class Webauth extends CI_Controller {
             try {
                 (new WebGuard())->validate($token);
 
-                $client_roles = (new WebGuard)->user()->client_roles;
-
-                // NOTE: You maybe want to get roles from your database by using $client_roles
-                // and put permissions to each role.
-                // Here's is example of result.
-                $roles = json_decode(json_encode([
-                    [
-                        'id' => 1,
-                        'name' => 'Operator',
-                        'permissions' => [
-                            'user:view',
-                            'user:edit',
-                        ]
-                    ],
-                    [
-                        'id' => 2,
-                        'name' => 'User',
-                        'permissions' => [
-                            'profile:view',
-                            'profile:edit',
-                        ]
-                    ],
-                ]));
-                
-                $serialize_session = serialize(array(
-                    'roles' => $roles,
-                    'role' => $roles[0],
-                ));
-
-                // PHP_SESSION_NONE if sessions are enabled, but none exists.
-                // https://www.php.net/manual/en/function.session-status.php
-                if (session_status() === PHP_SESSION_NONE) {
-                    session_start();
-                }
-
-                $_SESSION['serialize_session'] = $serialize_session;
+                // You may need to create a custom session for your internal app
+                $this->createSession();
 
                 redirect('/home');
             } catch (\Exception $e) {
                 throw new CallbackException($e->getCode(), $e->getMessage());
             }
         }
+    }
+
+    public function impersonate()
+    {
+        $username = $this->input->post('username');
+        $credentials = (new SSOService())->retrieveToken();
+        try {
+            $token = (new SSOService())->impersonate($credentials, $username);
+
+            if (empty($token)) {
+                throw new Exception("User with username {$username} not found!", 404);
+            }
+            
+            (new WebGuard())->validate($token);
+
+            $this->createSession();
+
+            redirect('/home');
+        } catch (\Throwable $th) {
+            echo "Status code: {$th->getCode()} \n";
+            echo "Error message: {$th->getMessage()}\n";
+            die();
+        }
+    }
+
+    private function createSession()
+    {
+        $client_roles = (new WebGuard)->user()->client_roles;
+        // NOTE: You maybe want to get roles from your database by using $client_roles
+        // and put permissions to each role.
+        // Here's is example of result.
+        $roles = json_decode(json_encode([
+            [
+                'id' => 1,
+                'name' => 'Operator',
+                'permissions' => [
+                    'user:view',
+                    'user:edit',
+                ]
+            ],
+            [
+                'id' => 2,
+                'name' => 'User',
+                'permissions' => [
+                    'profile:view',
+                    'profile:edit',
+                ]
+            ],
+        ]));
+        
+        $serialize_session = serialize(array(
+            'roles' => $roles,
+            'role' => $roles[0],
+        ));
+
+        // PHP_SESSION_NONE if sessions are enabled, but none exists.
+        // https://www.php.net/manual/en/function.session-status.php
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $_SESSION['serialize_session'] = $serialize_session;       
     }
 
     /**
