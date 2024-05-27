f0f2cfc7a1e64c8df9cb487e8b8f2080a8bdedfd
Kresna Satya <kresnasatya72@gmail.com>
2024-05-27 13:34:25 +0800

    fix parameter position to call impersonate() method
    This commit remove error message from PHP 8.1:
    
    Optional parameter $credentials declared before required parameter $username is implicitly treated as a required parameter

--- src/Services/SSOService.php
+++ src/Services/SSOService.php
@@ -540,10 +540,10 @@ class SSOService
      * 1. Enable feature Token Exchange, Fine-Grained Admin Permissions, and Account Management REST API in Keycloak.
      * 2. Register user(s) as impersonator in impersonate scope user permissions.
      * 
-     * @param credentials (access token of impersonator), username
+     * @param username, credentials (access token of impersonator)
      * @return array|exception
      */
-    public function impersonate($credentials = array(), $username)
+    public function impersonate($username, $credentials = array())
     {
         $token = [];
         
