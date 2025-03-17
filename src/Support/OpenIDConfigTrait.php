f8651458a48bcae189c879086f938fb913bb97b3
Kresna Satya <halo@kresna.me>
2025-03-17 08:16:34 +0800

    fix: add hasCache and forgetCache in OpenIDConfig trait

--- src/Support/OpenIDConfigTrait.php
+++ src/Support/OpenIDConfigTrait.php
@@ -7,6 +7,31 @@ trait OpenIDConfigTrait {
     protected $cache = [];
     protected $expiry = [];
 
+    public function hasCache($key)
+    {
+        if (!array_key_exists($key, $this->cache)) {
+            return false;
+        }
+
+        if (time() > $this->expiry[$key]) {
+            $this->forgetCache($key);
+            return false;
+        }
+
+        return true;
+    }
+
+    // Delete item from cache
+    public function forgetCache($key)
+    {
+        if (array_key_exists($key, $this->cache)) {
+            unset($this->cache[$key]);
+            unset($this->expiry[$key]);
+            return true;
+        }
+        return false;
+    }
+
     public function getCache($key, $default = null)
     {
         if ($this->hasCache($key)) {
